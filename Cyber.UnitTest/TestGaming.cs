b853b426590bd25378e2edf7c1a02e290ccf3ae2
titanix38 <telefonefix@hotmail.com>
2021-11-15 20:18:23 +0100

    Test for Gaming

--- Cyber.UnitTest/TestGaming.cs
+++ Cyber.UnitTest/TestGaming.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Linq;
 using System.Resources;
@@ -26,85 +27,14 @@ namespace Cyber.UnitTest
         [TestMethod]
         public void TestDice10Launch()
         {
-            int result;
+            // TODO : Faire le test pour le dé de 10
 
-            //int[] ts = new int[]
-            //{
-            //    7,-10,13,8,4,-7,-12,-3,3,6,-1,-6,7
-            //};
-
-            //int[] ts = new int[]
-            //{
-            //    -273
-            //};
-
-            //int[] ts = new int[]
-            //{
-            //    7,5,9,1,4
-            //};
-
-            //int[] ts = new int[]
-            //{
-            //    5526
-            //};
-
-            int[] ts = new int[]
-            {
-                15,-7,9,14,7,12
-            };
-
-            //int[] ts = new int[]
-            //{
-            //    -10,-10
-            //};
-
-
-            if (ts == null || ts.Length == 0) result = 0;
-            Array.Sort(ts);
-
-            int tneg = -0;
-            int tpos = 0;
-
-            int nbNeg = 0;
-            int nbPos = 0;
-
-            if (ts.Length > 10000) Array.Copy(ts, new int[10000], 10000);
-
-
-            for (int i = 0; i < ts.Length; i++)
-            {
-                if (ts[i] < 0)
-                {
-                    tneg = (int)Math.Abs((decimal)ts[i]);
-                    nbNeg++;
-                }
-                else
-                {
-                    tpos = ts[i];
-                    nbPos++;
-                    break;
-                }
-
-            }
-
-            if (tneg == tpos) result = tpos;
-
-            if (nbNeg == 0) result = tpos;
-            else if (nbPos == 0) result = -tneg;
-            else if (tneg < tpos) result = -tneg;
-            else result = tpos;
-
-            //if (nbNeg > 0 && tneg < tpos) result = -tneg;
-
-            //else if (tneg < -273 || tneg < tpos) result = -tneg;
-            //else if (tpos != int.MaxValue) result = tpos;
-            //return tpos;
         }
 
         [TestMethod]
         public void TestTempNearZero()
         {
-            int[] ts = new []{ 15 ,- 7, 9, 14, 7, 12 };
+            int[] ts = new[] { 15, -7, 9, 14, 7, 12 };
 
             int result = ComputeClosestToZero(ts);
         }
@@ -154,10 +84,6 @@ namespace Cyber.UnitTest
         {
             bool res1 = Exists(new[] { -9, 14, 37, 102 }, 102);
             bool res2 = Exists(new[] { -9, 14, 37, 102 }, 36);
-
-
-
-
         }
 
 
@@ -188,7 +114,7 @@ namespace Cyber.UnitTest
 
         [TestMethod]
 
-        public void Method() 
+        public void Method()
         {
             //int[] numbers = new int[]{1,2,3,4,5,6};
             //int[] threeNumbers = numbers[4..^1];
@@ -294,7 +220,7 @@ namespace Cyber.UnitTest
             {
                 "Ab","bcZ"
             };
-            ComputeCheckDigit(strs);
+            //ComputeCheckDigit(strs);
 
         }
         [TestMethod]
@@ -330,33 +256,121 @@ namespace Cyber.UnitTest
 
 
 
-        private static string ComputeCheckDigit(string[] parts)
+        private static string ComputeCheckDigit(string identificationNumber)
         {
             // Write your code here
             // To debug: Console.Error.WriteLine("Debug messages...");
+            char[] parts = identificationNumber.ToCharArray();
 
-            char[] p1 = parts[0].ToCharArray();
-            char[] p2 = parts[1].ToCharArray();
+            List<int> pair = new List<int>();
+            List<int> impair = new List<int>(); ;
 
 
-            string first1 = p1[0].ToString();
-            string final1 = p1[p1.Length - 1].ToString();
-
-            string first2 = p2[0].ToString();
-            string final2 = p2[p2.Length - 1].ToString();
-            string fir = string.Concat(p1);
-            string sec = string.Concat(p2);
-
-            if (final1 == first2)
+            for (int i = 0; i < parts.Length; i++)
             {
-                sec = sec.Substring(1);
-            }
+                if (i % 2 == 0) pair.Add(int.Parse(parts[i].ToString())); else impair.Add(int.Parse(parts[i].ToString()));
 
-            return string.Concat(fir, sec);
+            }
+            int somme = pair.Sum(item => item);
+
+
+
+            throw new Exception();
 
 
 
         }
+        [TestMethod]
+        public void TestDataCenter()
+        {
+            string dir = "U";
+
+            int[] resInts = Solve("U", 0, 0, 1000, 1000);
+        }
+
+
+        private static int[] Solve(string direction, int x, int y, int width, int height)
+        {
+            // Write your code here
+            // To debug: Console.Error.WriteLine("Debug messages...");
+            int[] coord = new int[width];
+            int x1;
+            int y1;
+            switch (direction)
+            {
+                case "U":
+                    if (y > 1) coord[x] = y - 1; else coord[x] = y;
+                    break;
+
+                case "UR":
+
+
+                    if (x < width)
+                    {
+                        x1 = x + 1;
+                    }
+                    else { x1 = 0; }
+                    if (y > 0) { y1 = y - 1; }
+                    else { y1 = 0; }
+                    coord[x1] = y1;
+
+                    break;
+
+                case "R":
+                    if (x < width) x1 = x + 1;
+                    else x1 = width;
+
+                    coord[x1] = y;
+
+                    break;
+
+                case "DR":
+
+                    if (x < width) { x1 = x + 1; } else { x1 = width; }
+                    if (y < height) { y1 = y + 1; } else { y1 = height; }
+
+                    coord[x1] = y1;
+
+
+                    break;
+
+                case "D":
+                    if (y < height) { y1 = y + 1; } else { y1 = height; }
+                    coord[x] = y1;
+                    break;
+
+                case "DL":
+                    if (x > 0) { x1 = x - 1; } else { x1 = width; }
+                    if (y < height) { y1 = y + 1; } else { y1 = height; }
+
+
+                    coord[x1] = y1;
+
+                    break;
+
+                case "L":
+                    if (x > 0) { x1 = x - 1; } else { x1 = width; }
+                    coord[x1] = y;
+
+                    break;
+
+                case "UL":
+
+                    if (y > 1) y1 = y - 1; else y1 = y;
+                    if (x > 0) { x1 = x - 1; } else { x1 = width; }
+
+                    coord[x1] = y1;
+
+                    break;
+
+
+
+            }
+
+
+            return null;
+        }
+
 
         [TestMethod]
         public void TestDuoDigit()
@@ -372,7 +386,7 @@ namespace Cyber.UnitTest
             return (n * (n - 1) / 2);
 
         }
-        
+
         private string IsDuoDigit(int number)
         {
 
@@ -382,7 +396,7 @@ namespace Cyber.UnitTest
 
             //Array.Sort(num);
 
-          
+
 
 
             char[] outChar = num.Distinct().ToArray();
